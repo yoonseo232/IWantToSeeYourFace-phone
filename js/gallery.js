cbc77a549d8322333001a94e192497bfd82276eb
yoonseo232 <ys.bang232@gmail.com>
2024-06-06 19:39:54 +0900

    주소록 완성

--- js/gallery.js
+++ js/gallery.js
@@ -6,16 +6,17 @@ const imageDescription = document.querySelector('.image-description');
 const images = [
     { src: 'images/galleryImg/img1.png', description: '솔로 엔딩 초기 시안' },
     { src: 'images/galleryImg/img2.png', description: '민들레 초기 디자인 시안' },
-    { src: 'images/galleryImg/img3.png', description: '작년 프로젝트 타이틀 김여주 (초기)' },
-    { src: 'images/galleryImg/img4.png', description: '작년 프로젝트 타이틀 김여주 (확정)' },
+    { src: 'images/galleryImg/img3.png', description: '전 프로젝트 타이틀 김여주 (초기)' },
+    { src: 'images/galleryImg/img4.png', description: '전 프로젝트 타이틀 김여주 (확정)' },
     { src: 'images/galleryImg/img5.png', description: '? 엔딩 초기 시안' },
-    { src: 'images/galleryImg/img6.gif', description: '작년 프로젝트 로딩 gif' },
+    { src: 'images/galleryImg/img6.gif', description: '전 프로젝트 로딩 gif' },
     { src: 'images/galleryImg/img7.png', description: '장미고 로고' },
-    { src: 'images/galleryImg/img8.png', description: '작년 프로젝트 로고' },
+    { src: 'images/galleryImg/img8.png', description: '전 프로젝트 로고' },
     { src: 'images/galleryImg/img9.png', description: '? 엔딩 초기 시안' },
     { src: 'images/galleryImg/img10.png', description: '? 엔딩 초기 시안' },
-    { src: 'images/galleryImg/img11.png', description: '작년 프로젝트 메인화면' },
-    { src: 'images/galleryImg/img12.png', description: '홍보용 제작 이미지' }
+    { src: 'images/galleryImg/img11.png', description: '전 프로젝트 메인화면' },
+    { src: 'images/galleryImg/img12.png', description: '홍보용 제작 이미지' },
+    { src: 'images/galleryImg/img13.png', description: '지연 메세지 프로필' }
 ];
 
 let currentImageIndex = 0;
