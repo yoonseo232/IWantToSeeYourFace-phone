8b7758fa1cb6072ec9fb1eb5ae8a47d47e893b46
yoonseo232 <ys.bang232@gmail.com>
2024-06-18 10:52:40 +0900

    gallery 추가

--- js/gallery.js
+++ js/gallery.js
@@ -4,19 +4,24 @@ const imageContainer = document.querySelector('.image-container');
 const imageDescription = document.querySelector('.image-description');
 
 const images = [
-    { src: 'images/galleryImg/img1.png', description: '솔로 엔딩 초기 시안' },
-    { src: 'images/galleryImg/img2.png', description: '민들레 초기 디자인 시안' },
+    { src: 'images/galleryImg/img6.gif', description: '전 프로젝트 로딩 gif' },
     { src: 'images/galleryImg/img3.png', description: '전 프로젝트 타이틀 김여주 (초기)' },
     { src: 'images/galleryImg/img4.png', description: '전 프로젝트 타이틀 김여주 (확정)' },
-    { src: 'images/galleryImg/img5.png', description: '? 엔딩 초기 시안' },
-    { src: 'images/galleryImg/img6.gif', description: '전 프로젝트 로딩 gif' },
-    { src: 'images/galleryImg/img7.png', description: '장미고 로고' },
+    { src: 'images/galleryImg/img11.png', description: '전 프로젝트 메인화면' },
     { src: 'images/galleryImg/img8.png', description: '전 프로젝트 로고' },
+    { src: 'images/galleryImg/img7.png', description: '장미고 로고' },
+    { src: 'images/galleryImg/img17.png', description: '전 프로젝트 ver.1 초상화' },
+    { src: 'images/galleryImg/img18.png', description: '(드랍)프로필 패널 디자인' },
+    { src: 'images/galleryImg/img2.png', description: '민들레 초기 디자인 시안' },
+    { src: 'images/galleryImg/img1.png', description: '솔로 엔딩 초기 시안' },
+    { src: 'images/galleryImg/img5.png', description: '? 엔딩 초기 시안' },
     { src: 'images/galleryImg/img9.png', description: '? 엔딩 초기 시안' },
     { src: 'images/galleryImg/img10.png', description: '? 엔딩 초기 시안' },
-    { src: 'images/galleryImg/img11.png', description: '전 프로젝트 메인화면' },
+    { src: 'images/galleryImg/img16.png', description: '? 엔딩 초기 시안' },
     { src: 'images/galleryImg/img12.png', description: '홍보용 제작 이미지' },
-    { src: 'images/galleryImg/img13.png', description: '지연 메세지 프로필' }
+    { src: 'images/galleryImg/img13.png', description: '지연 메세지 프로필' },
+    { src: 'images/galleryImg/img14.png', description: '(드랍)오프닝 전 사용 예정이었던1' },
+    { src: 'images/galleryImg/img15.png', description: '(드랍)오프닝 전 사용 예정이었던2' }
 ];
 
 let currentImageIndex = 0;
--- js/reset.js
+++ js/reset.js
@@ -1,9 +1,11 @@
+const hostname = ''
+
 function checkReset() {
     fetch('/reset')
         .then(response => response.json())
         .then(data => {
             if (data.reset) {
-                 window.location.href = '/';
+                 window.location.href = `http://${hostname}:4000/`;
             }
         })
         .catch(error => console.error('Error:', error));
